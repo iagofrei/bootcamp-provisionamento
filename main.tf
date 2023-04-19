0770ae3a57446fb1ab6a5dc188a254ecb73e347e
Iago Freitas <iago.g.freitas@gmail.com>
2023-04-19 17:57:14 -0300

    ajustes pós aula

--- main.tf
+++ main.tf
@@ -112,3 +112,66 @@ resource "aws_lambda_permission" "invoke_function" {
   depends_on = [aws_lambda_function.lambda_gp3]
 }
 
+# --------------------------------------------------------------------- VPC -----------------------------------------------
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "172.16.1.0/25" # o /25 indica a quantidade de IPs disponíveis para máquinas na rede
+
+  tags = {
+    Name = "VPC-1-DE-OP-009-gp3"
+  }
+}
+
+
+resource "aws_subnet" "private-subnet" {
+  count             = var.subnet_count
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = var.subnet_cidr_block[count.index] # "172.16.1.0/25" 172.16.1.48 até 172.16.1.64 
+  availability_zone = var.subnet_availability_zone[count.index]
+
+  tags = {
+    Name = "Subnet-${count.index + 1}-DE-OP-009-gp3"
+  }
+}
+
+
+resource "aws_db_subnet_group" "db-subnet" {
+  name       = "db_subnet_group_gp3"
+  subnet_ids = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id]
+}
+
+
+resource "aws_security_group" "allow_db" {
+  name        = "permite_conexao_rds"
+  description = "Grupo de seguranca para permitir conexao ao db"
+  vpc_id      = aws_vpc.dev-vpc.id
+
+  ingress {
+    description = "Porta-conexao-padrao-postgres"
+    from_port   = var.numero_da_porta
+    to_port     = var.numero_da_porta
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.dev-vpc.cidr_block] # aws_vpc.dev-vpc.cidr_blocks
+  }
+
+  tags = {
+    Name = "DE-OP-009-gp3"
+  }
+}
+
+
+# --------------------------------------------------------------------- RDS -----------------------------------------------
+resource "aws_db_instance" "postgres" {
+  allocated_storage = var.producao ? 50 : 10
+  db_name           = "mydb"
+  identifier        = "mydb-gp3"
+  engine            = "postgres"
+  engine_version    = "12.9"
+  instance_class    = var.producao == true ? "db.t2.micro" : "db.t3.micro"
+  username          = "username" # Nome do usuário "master"
+  password          = "password" # Senha do usuário master
+  port              = var.numero_da_porta
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.db-subnet.name
+  vpc_security_group_ids = [aws_security_group.allow_db.id]
+}
--- variables.tf
+++ variables.tf
@@ -27,3 +27,33 @@ variable "retencao_logs" {
   default = 1
   description = "Número de dias de retenção dos logs."
 }
+
+variable "subnet_cidr_block" {
+    type = list
+    default = ["172.16.1.48/28", "172.16.1.64/28", "172.16.1.80/28"]
+    description = "CIDR block as subnets"
+}
+
+variable "subnet_availability_zone" {
+  type = list
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  description = "AZ para as subnets"
+}
+
+variable "subnet_count" {
+  type = number
+  default = 3
+  description = "Número de subnets a serem criadas"
+}
+
+variable "producao" {
+  type = bool
+  default = false
+  description = "Variável que indica se as configs são de produção ou não"
+}
+
+variable "numero_da_porta" {
+  type = number
+  default = 5432
+  description = "Numero da nossa porta"
+}
